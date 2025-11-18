7f780c263f442ce4be6764a290a4ea475767f77f
rosee69 <tatyanaviktoriya12@gmail.com>
2025-11-18 20:40:33 +0200

    Configure products app (views, urls, installed apps)

--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponce
 
 # Create your views here.
+def index(request):
+    return HttpResponce("Welcome to the products of our store!");
