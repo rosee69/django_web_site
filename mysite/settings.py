c7207187614e2f0cc1d20a6afa012ad34317a805
rosee69 <tatyanaviktoriya12@gmail.com>
2026-01-09 00:10:00 +0200

    updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://django-web-site.onrender.com",
