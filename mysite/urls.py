05398470044dc5b69cfe234c9704952b8e3fae33
rosee69 <tatyanaviktoriya12@gmail.com>
2026-01-09 04:29:59 +0200

    updated

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,7 +7,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secure-admin/', admin.site.urls),
 
 
     # Token auth
